58edd7fd5d165f5ea26bcad2b003db810d26fec8
Nikolay Nagorskiy <nikolaynag@gmail.com>
2015-12-13 02:14:30 +0300

    First working version: quick and dirty

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tachistoscope
+
+Perform tests showing different words for short period of time. Tests are defined in tests.js file. You can set a list of words, a delay between mouse click and word presentation and presentation time in milliseconds.
---
+++ tests.js
@@ -0,0 +1,27 @@
+var tests = [
+    {
+        title: "Test one",
+        enabled: false, //you can disalbe test if you want
+        words: ["word", "lord", "sword", "irina", "tachistoscope"],
+        exposition: 500, //word exposition time in milliseconds
+        delay: 100,// delay in milliseconds between click and word presentation
+    },
+    {
+        title: "Тест",
+        words: ["весло", "луна", "огонь", "вода", "тахистоскоп!"],
+        exposition: 40,
+        delay: 100,
+    },
+    {
+        title: "Долгий показ",
+        words: ["весло", "луна", "огонь", "вода", "тахистоскоп!"],
+        exposition: 400,
+        delay: 100,
+    },
+    {
+        title: "Большая задержка",
+        words: ["весло", "луна", "огонь", "вода", "тахистоскоп!"],
+        exposition: 40,
+        delay: 500,
+    },
+];
